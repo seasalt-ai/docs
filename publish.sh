6c31f17bf26d9d045e8fb107ebc0fe41ada63246
Xuchen Yao <yaoxuchen@gmail.com>
2021-11-03 23:27:05 -0700

    update _static

--- publish.sh
+++ publish.sh
@@ -8,8 +8,10 @@ make clean; make html
 if [[ "$OSTYPE" == "linux-gnu" ]]
 then
     cp -rf build/html/* ${DOCS_FOLDER}/
+    rm -rf _static && mv -f ${DOCS_FOLDER}/_static .
 else
     /usr/local/opt/coreutils/libexec/gnubin/cp -rf build/html/* ${DOCS_FOLDER}/
+    /usr/local/opt/coreutils/libexec/gnubin/rm -rf _static && /usr/local/opt/coreutils/libexec/gnubin/mv -f ${DOCS_FOLDER}/_static .
 fi 
 
 echo "now please commit your changes and submit a PR"
