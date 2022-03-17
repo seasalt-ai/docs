f30068c4907eee02f49bd9c5af0010a86bb9a7c6
shaynemei <shayne.mei@aiesec.net>
2022-03-17 13:28:51 -0700

    fix publish script bug

--- publish.sh
+++ publish.sh
@@ -7,9 +7,9 @@ DOCS_FOLDER="./docs"
 make clean; make html
 if [[ "$OSTYPE" == "linux-gnu" ]]
 then
-    cp -rf build/html/* ${DOCS_FOLDER}/
-else
     /usr/local/opt/coreutils/libexec/gnubin/cp -rf build/html/* ${DOCS_FOLDER}/
+else
+    cp -rf build/html/* ${DOCS_FOLDER}/
 fi 
 
 echo "now please commit your changes and submit a PR"
